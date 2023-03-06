db5c876595cbac3aa2de1e3d5277b64003accacf
Karlos Aguirre <karlos.dev416@gmail.com>
2023-03-06 15:40:02 -0600

    Initial project

---
+++ QuizApp/View Models/QuizGradeViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  GradeViewModel.swift
+//  QuizApp
+//
+//  Created by Mohammad Azam on 10/22/21.
+//
+
+import Foundation
+
+class QuizGradeViewModel: ObservableObject {
+    
+    var networkService: NetworkService
+    
+    init(networkService: NetworkService) {
+        self.networkService = networkService
+    }
+    
+    func submitQuiz(submission: QuizSubmission) {
+        
+        networkService.getQuizById(url: Constants.Urls.quizById(submission.quizId)) { result in
+            switch result {
+                case .success(let quizDTO):
+                    print(quizDTO)
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
+    
+}
+
+
